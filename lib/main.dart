90c40003982c1c3f5234d89bed10f26268780c82
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-04-06 18:37:03 -0400

    added changes to main.dart like imports and streambuilder.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
+import 'data/items_database.dart';
+import 'item.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,13 +37,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final ItemDatabase service = ItemDatabase();
 
   @override
   Widget build(BuildContext context) {
@@ -50,21 +46,51 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: StreamBuilder<List<Item>>(
+        stream: service.streamItems(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          final items = snapshot.data ?? [];
+
+          if (items.isEmpty) {
+            return const Center(child: Text('No items yet.'));
+          }
+
+          return ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (_, i) {
+              final item = items[i];
+
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text(item.name),
+                  subtitle: Text(
+                    'Qty: ${item.quantity} | \$${item.price}',
+                  ),
+
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => service.deleteItem(item.id!),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
+
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          //editing next
+        },
         child: const Icon(Icons.add),
       ),
     );
