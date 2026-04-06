9cc26b975716eb18985dcff20695891ccae182b6
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-04-06 18:28:57 -0400

    items service/database created.

---
+++ lib/data/items_database.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../item.dart';
+
+class ItemDatabase {
+  final CollectionReference itemsRef = FirebaseFirestore.instance.collection('items');
+
+  Future<void> addItem(Item item) async { //creates
+    await itemsRef.add(item.toMap());
+  }
+
+  Stream<List<Item>> streamItems() { //read from real-time stream
+    return itemsRef.snapshots().map(
+          (snap) => snap.docs
+              .map((d) =>
+                  Item.fromMap(d.id, d.data() as Map<String, dynamic>))
+              .toList(),
+        );
+  }
+
+  Future<void> updateItem(Item item) async { //update
+    await itemsRef.doc(item.id).update(item.toMap());
+  }
+
+  Future<void> deleteItem(String id) async {
+    await itemsRef.doc(id).delete();
+  }
+}
+
